a4483e684be4ef453b26f9e724baf7d15084972e
SinaTamjid <91365237+SinaTamjid@users.noreply.github.com>
2025-03-11 13:27:59 +0330

    version 1.0

--- model.py
+++ model.py
@@ -2,10 +2,11 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error,r2_score
+from sklearn.metrics import r2_score
+import joblib
 
 #Reading Dataset & Droping Useless Columns
-df=pd.read_csv(r'py\NLP\House-Prices-Prediction\data\house_data.csv')
+df=pd.read_csv(r'House-Prices-Prediction/data/house_data.csv')
 df=df.drop(columns=['date','id','zipcode'],axis=1)
 df_sampled = df.sample(frac=0.1, random_state=42)  # Use 10% of the data
 
@@ -17,17 +18,19 @@ y=df['price']
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
 
 #calling The model
-# lr=LogisticRegression(random_state=42)
-RFR=RandomForestRegressor(random_state=42)
+
+RFR = RandomForestRegressor(n_estimators=500, max_depth=10, random_state=42)
+
 
 #fiting the training data
 
 RFR.fit(X_train,y_train)
 
-#make prediction on our test data
+#saving the model
+joblib.dump(RFR,r"House-Prices-Prediction/model/model.pkl",compress=9)
 
+#make prediction on our test data
 pred=RFR.predict(X_test)
 
 #evaluate the model on his own data
 print(f"accuracy is :{r2_score(y_test,pred)}")
-print(f"MSE is : {mean_squared_error(y_test,pred)}")
